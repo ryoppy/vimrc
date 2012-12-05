3d55c178faa83c73badc33aa69783d62bccdfbd3
ryo <xxyba516@yahoo.co.jp>
2012-12-05 15:40:39 +0900

    インストーラーでsubmoduleを落とすように修正

--- auto-install.sh
+++ auto-install.sh
@@ -15,12 +15,28 @@ cd ~/.vim
 # Get Vimrc
 git clone git://github.com/ryoppy/vimrc.git .
 
+# Get Submodule
+git submodule update --init
+
+# phplintのプラグインがplugin/の中にない...これどうしよ...一旦コピーするか
+mkdir bundle/vim-phplint/plugin
+cp bundle/vim-phplint/phplint.vim bundle/vim-phplint/plugin
+
 # Set .vimrc
 ln -sf ~/.vim/.vimrc ~/
 ln -sf ~/.vim/.jslintrc ~/
 
-echo "Vim setup complete."
-echo ""
+echo "--------------------------------------"
+echo "STEP.1 vim-proc compile."
+echo "--------------------------------------"
+echo "[mac]"
+echo "cd bundle/vim-proc/ && make -f make_mac.mak"
+echo "[unix]"
+echo "cd bundle/vim-proc/ && make -f make_unix.mak"
+
+echo "--------------------------------------"
+echo "COMPELTE!"
+echo "--------------------------------------"
 echo "--- directory ---"
 echo " ~/.vim"
 echo "--- symbolic link ---"
